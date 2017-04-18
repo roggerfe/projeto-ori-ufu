bc522bd620aff490e423a7d6b4ee929e6e9d640b
Rogger Fernandes <rogger.fernandes10@gmail.com>
2017-04-18 18:50:18 -0300

    classe termo

---
+++ src/main/java/br/ufu/ori/models/Termo.java
@@ -0,0 +1,68 @@
+package br.ufu.ori.models;
+
+public class Termo {
+	
+	private String nome;
+	private Integer quantidade;
+	
+	public Termo(String nome) {
+		super();
+		this.nome = nome;
+		this.quantidade = 1;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((quantidade == null) ? 0 : quantidade.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Termo other = (Termo) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (quantidade == null) {
+			if (other.quantidade != null)
+				return false;
+		} else if (!quantidade.equals(other.quantidade))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Termo [nome=" + nome + ", quantidade=" + quantidade + "]";
+	}
+	
+	
+
+}
