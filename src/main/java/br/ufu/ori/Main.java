8f6881a3db5839b892dd26e798f2537c665e0046
Rogger Fernandes <rogger.fernandes10@gmail.com>
2017-04-18 19:03:23 -0300

    parametro main

---
+++ src/main/java/br/ufu/ori/Main.java
@@ -0,0 +1,13 @@
+package br.ufu.ori;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		if (args.length == 0) {
+			System.err.println("Passar como parametro caminho completo do arquivo de entrada.");
+			System.exit(1);
+		}
+	}
+}
